fc35c94708aa319522061f945bb558cce9015db5
wyl <1653152776@qq.com>
2019-06-20 21:48:24 +0800

    信息

---
+++ aigou_palt_parent/aigou_palt_serve2000/src/test/java/com/wyl/aigou/AppTest.java
@@ -0,0 +1,20 @@
+package com.wyl.aigou;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple PaltServeApplication.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
